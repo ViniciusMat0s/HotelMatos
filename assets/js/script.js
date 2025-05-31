f5e526efa9fc3abbb5193ea92fa26cd3452ca5db
ViniciusMat0s <vinibmatos@rede.ulbra.br>
2025-05-31 09:11:35 -0300

    mediaquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,13 @@ function enviarMensagem(event) {
 
     window.open(url, "_blank")
 }
+
+window.onload = function(){
+	document.querySelector(".menuMobile").addEventListener("click", function(){
+		if(document.querySelector(".menu nav ul").style.display == 'flex') {
+			document.querySelector(".menu nav ul").style.display = 'none';
+		} else {
+			document.querySelector(".menu nav ul").style.display = 'flex';
+		}
+	});
+};
